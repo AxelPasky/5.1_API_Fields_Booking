be0b2c048e88796fc4426aafb9d00850b5eccff2
Alex Pasquero <axelpasquero@gmail.com>
2025-07-03 12:55:25 +0200

    Added route /logout

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,10 @@ Route::post('/login', [AuthController::class, 'login']);
 // Rotta di registrazione che punta al nostro controller
 Route::post('/register', [AuthController::class, 'register']);
 
-// Esempio di rotta protetta
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Rotte protette che richiedono autenticazione
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
