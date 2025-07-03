4dbbc8ad111d04abbad921ffd0aad2cd98fc67b0
Alex Pasquero <axelpasquero@gmail.com>
2025-07-03 13:17:54 +0200

    Added function user on AuthContoller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -73,4 +73,9 @@ class AuthController extends Controller
 
         return response()->json(['message' => 'Logged out successfully']);
     }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
 }
