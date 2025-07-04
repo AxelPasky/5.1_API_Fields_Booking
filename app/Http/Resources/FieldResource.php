1a92653a4b679f97e588edcb997507cce695bb71
Alex Pasquero <axelpasquero@gmail.com>
2025-07-04 11:12:13 +0200

    Added FieldResource

---
+++ app/Http/Resources/FieldResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FieldResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_available' => $this->is_available,
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
